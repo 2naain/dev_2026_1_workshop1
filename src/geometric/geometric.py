94f0c80a8a9cb0aeded8f5cf10c48511bd68e90e
2naain <jdlozano31@ucatolica.edu.co>
2026-03-11 23:07:57 -0500

    esferaa del dragao

--- src/geometric/geometric.py
+++ src/geometric/geometric.py
@@ -220,7 +220,7 @@ class Geometria:
         Returns:
             float: Área de la superficie del cubo
         """
-        return float(6 * lado ** 2) s
+        return float(6 * lado ** 2) 
         pass
     
     def volumen_esfera(self, radio):
