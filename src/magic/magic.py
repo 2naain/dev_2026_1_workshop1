951e7ef52a8c0397907b8f6ca79bb0df14342d98
2naain <jdlozano31@ucatolica.edu.co>
2026-03-12 12:19:11 -0500

    finally

--- src/magic/magic.py
+++ src/magic/magic.py
@@ -20,11 +20,12 @@ class Magic:
             return 0
         if n == 1:
             return 1
+
         a, b = 0, 1
-        for _ in  range(2, n + 1):
-            a, b = b, a +  b
-            return b
-what happends
+        for _ in range(2, n + 1):
+            a, b = b, a + b
+        return b
+
         pass
     
     def secuencia_fibonacci(self, n):
