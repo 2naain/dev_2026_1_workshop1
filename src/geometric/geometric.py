0f58fa6da0fe207ec70d4adf30041f9539ba58a1
2naain <xovxx.xv@gmail.com>
2026-03-11 22:54:27 -0500

    valid triangle

--- src/geometric/geometric.py
+++ src/geometric/geometric.py
@@ -109,6 +109,7 @@ class Geometria:
         Returns:
             bool: True si los lados pueden formar un triángulo, False en caso contrario
         """
+        return (lado1 + lado2 > lado3) and (lado1 + lado3 > lado2) and (lado2 + lado3 > lado1
         pass
     
     def area_trapecio(self, base_mayor, base_menor, altura):
