f83ffcc03b763a7d03e4eb1e781dc3ced2b52587
2naain <xovxx.xv@gmail.com>
2026-03-11 22:44:08 -0500

    perimetro

--- src/geometric/geometric.py
+++ src/geometric/geometric.py
@@ -31,6 +31,7 @@ class Geometria:
         Returns:
             float: Perímetro del rectángulo
         """
+        return float(2  (base + altura))
         pass
     
     def area_circulo(self, radio):
