2b244d37e52283f63181b6d0e0202354913c14d3
2naain <jdlozano31@ucatolica.edu.co>
2026-03-12 12:16:55 -0500

    trying fibonaccci

--- src/magic/magic.py
+++ src/magic/magic.py
@@ -14,7 +14,17 @@ class Magic:
         Returns:
             int: El n-ésimo número de Fibonacci
         """
-        fibonacci was my first nightmare on first semester, I hope I can do it right this time :X
+        if n < 0:
+            return None
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        a, b = 0, 1
+        for _ in  range(2, n + 1):
+            a, b = b, a +  b
+            return b
+
         pass
     
     def secuencia_fibonacci(self, n):
