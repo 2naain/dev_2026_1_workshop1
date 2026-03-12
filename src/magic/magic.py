edaf0b77430f8fda88fbaed3575938d7496f490e
2naain <jdlozano31@ucatolica.edu.co>
2026-03-12 12:30:27 -0500

    abora no funciona num perffect

--- src/magic/magic.py
+++ src/magic/magic.py
@@ -131,6 +131,13 @@ class Magic:
         Returns:
             int: El factorial de n
         """
+        if n < 0:
+            return None
+        if n == 0 or n == 1:
+            return 1
+        resultado = 1
+        for i in range(2, n + 1):
+            resultado *= i
         pass
     
     def mcd(self, a, b):
