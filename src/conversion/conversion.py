51ea98ddb0ee227074714397a9b02a7bde01dfa8
2naain <jdlozano31@ucatolica.edu.co>
2026-08-20 18:53:00 -0500

    comenzando test conversiones

--- src/conversion/conversion.py
+++ src/conversion/conversion.py
@@ -2,7 +2,7 @@ class Conversion:
     
     def celsius_a_fahrenheit(self, celsius: float):
         farenheit = (celsius * 9/5) + 32
-        return farenheit
+        return farenheitsdsd
 
         """
         Convierte temperatura de Celsius a Fahrenheit.
